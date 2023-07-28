36f58f3b4200d3652fc463c48b4fddf7aa030035
gimgitae <rlxo6919@chicment.com>
2023-07-28 22:26:48 +0900

    first

---
+++ src/main/java/testyml/testyml/TestYmlApplication.java
@@ -0,0 +1,13 @@
+package testyml.testyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestYmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testyml/testyml/TestYmlApplicationTests.java
@@ -0,0 +1,13 @@
+package testyml.testyml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
